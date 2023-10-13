c0a78b4f531b158daa1f1faacc6a261e4fde0fba
linggarpa <linggarpramudia24@gmail.com>
2023-10-13 16:24:10 +0700

    mbahkan validasi data dengan form validasi pada controller

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Prog II | Merancang Template sederhana dengan codeignater</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/stylebuku.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/stylebuku.css">```
 </head>
 <body>
     <div id="wrapper">
